495ec13a93947038c3c37806b8c2dd104ca48303
Jonathan Wright <jonathan.wright@allsaints.wa.edu.au>
2021-05-07 16:49:13 +0800

    added update endpoint

---
+++ UnitAdminAPI/updateunit/db.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+/*
+	Purpose: Return a error if the unit doesnt exists in DB, else return nil
+	TODO: (Maybe make use of panic here)
+*/
+func checkInDatabase(unit Unit, db *dynamodb.DynamoDB) error {
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String("DevUnits"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"UnitCode": {
+				S: aws.String(unit.UnitCode),
+			},
+		},
+	}
+	item, getErr := db.GetItem(input)
+	if getErr == nil {
+		if item.Item == nil {
+			return errors.New("that unitCode doesnt exist in db")
+		} else {
+			return nil
+		}
+	} else {
+		if aerr, ok := getErr.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				return errors.New("DBError: Provisioned Throughput has been exceeded")
+			case dynamodb.ErrCodeResourceNotFoundException:
+				return nil
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				return errors.New("DBError: cannot request from db, limit exceeded")
+			case dynamodb.ErrCodeInternalServerError:
+				return errors.New("DBError: internal server error (most likely on Amazon's Side)")
+			default:
+				return errors.New(aerr.Error())
+			}
+		} else {
+			return errors.New(getErr.Error())
+		}
+	}
+}
+
+/*
+	String sets require []*string, so I guess we need to convert it.
+	TODO: (Please phase this out, surely it doesnt need a array of pointers...)
+*/
+func convertToStringMemoryArray(arr []string) []*string {
+	var outArr []*string
+	for index, _ := range arr {
+		outArr = append(outArr, &arr[index])
+	}
+	return outArr
+}
+
+/*
+	Purpose: Return a error if the unit fails to add to db, else return nil
+	TODO: (Maybe make use of panic here)
+*/
+func addToDatabase(unit Unit, db *dynamodb.DynamoDB) error {
+	input := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"UnitCode": {
+				S: aws.String(unit.UnitCode),
+			},
+			"Name": {
+				S: aws.String(unit.Name),
+			},
+			"Description": {
+				S: aws.String(unit.Description),
+			},
+			"Credits": {
+				N: aws.String(fmt.Sprintf("%f", unit.Credits)),
+			},
+			"Delivery": {
+				S: aws.String(unit.Delivery),
+			},
+			"Prerequistes": {
+				SS: convertToStringMemoryArray(unit.Prerequistes),
+			},
+			"Corequistes": {
+				SS: convertToStringMemoryArray(unit.Corequistes),
+			},
+			"Antirequistes": {
+				SS: convertToStringMemoryArray(unit.Antirequistes),
+			},
+		},
+		TableName: aws.String("DevUnits"),
+	}
+
+	_, err := db.PutItem(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				return errors.New("dberror: conditionalcheck failed")
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				return errors.New("dberror: throughput exceeded")
+			case dynamodb.ErrCodeResourceNotFoundException:
+				return errors.New("dberror: resource not found")
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				return errors.New("dberror: FATAL, item collection size exceeded")
+			case dynamodb.ErrCodeTransactionConflictException:
+				return errors.New("dberror: transaction conflict")
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				return errors.New("dberror: request limits reached")
+			case dynamodb.ErrCodeInternalServerError:
+				return errors.New("dberror: dynamodb internal server error, likely aws down")
+			default:
+				return errors.New(aerr.Error())
+			}
+		} else {
+			return errors.New(err.Error())
+		}
+		return nil
+	}
+	return nil
+}
---
+++ UnitAdminAPI/updateunit/main.go
@@ -0,0 +1,57 @@
+package main
+
+// TODO: main_test.go
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+var db *dynamodb.DynamoDB
+
+type Unit struct {
+	UnitCode      string   `json:"unitCode"`
+	Name          string   `json:"unitName"`
+	Description   string   `json:"unitDescription"`
+	Credits       float32  `json:"unitCredits"` // could be a half unit :/
+	Delivery      string   `json:"delivery"`
+	Prerequistes  []string `json:"prerequistes"`
+	Corequistes   []string `json:"corequistes"`
+	Antirequistes []string `json:"antirequistes"`
+}
+
+func handler(ctx context.Context, payload Unit) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Recieved Payload: %+v\n", payload) // redirect to logs
+
+	inDBError := checkInDatabase(payload, db)
+	if inDBError == nil {
+		addDBError := addToDatabase(payload, db)
+		if addDBError != nil {
+			return events.APIGatewayProxyResponse{
+				Body:       "Failure To Add Unit To DB!",
+				StatusCode: 400,
+			}, addDBError
+		} else {
+			return events.APIGatewayProxyResponse{
+				Body:       "Successfully Updated Unit",
+				StatusCode: 200,
+			}, nil
+		}
+	} else {
+		return events.APIGatewayProxyResponse{
+			Body:       inDBError.Error(),
+			StatusCode: 400,
+		}, inDBError
+	}
+}
+
+func main() {
+	session := session.Must(session.NewSession())
+	db = dynamodb.New(session)
+	db.Config.WithRegion("ap-southeast-2")
+	lambda.Start(handler)
+}
