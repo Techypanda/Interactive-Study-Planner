94a34f8243d1d32d1382365744ce5cc3e8ddfe07
Techypanda <Jonathan_wright@hotmail.com>
2021-05-30 16:21:33 +0800

    Update dockerfiles with alpine (smaller images), now clean up ECR on push via BuildTestDeploy.sh

---
+++ UnitAdminAPI/BuildTestDeploy.sh
@@ -0,0 +1,5 @@
+sam build
+IMAGES_TO_DELETE=$( aws ecr list-images --region ap-southeast-2 --repository-name "curtinmedicalcourseplanner/unitadmin" --query 'imageIds[*]' --output json )
+aws ecr batch-delete-image --region ap-southeast-2 --repository-name curtinmedicalcourseplanner/unitadmin --image-ids "$IMAGES_TO_DELETE" || true
+sam package --output-template-file packaged-template.yaml --image-repository 363837338544.dkr.ecr.ap-southeast-2.amazonaws.com/curtinmedicalcourseplanner/unitadmin --no-progressbar
+sam deploy --template ./packaged-template.yaml --stack-name UnitAdminAPIDev --region ap-southeast-2 --capabilities CAPABILITY_IAM --image-repository 363837338544.dkr.ecr.ap-southeast-2.amazonaws.com/curtinmedicalcourseplanner/unitadmin
