cd7be5d367b939755aa1fb5d532f3f9e21dccfdf
Techypanda <Jonathan_wright@hotmail.com>
2021-05-31 20:34:35 +0800

    Pipeline now hopefully stops if error

--- UnitAdminAPI/BuildTestDeploy.sh
+++ UnitAdminAPI/BuildTestDeploy.sh
@@ -4,11 +4,15 @@ sam build
 echo "Testing API"
 ./RunSamTests.sh
 go test -v
-echo "Beginning Deploy Stage"
-echo "Wiping ECR Before Deploying..."
-IMAGES_TO_DELETE=$( aws ecr list-images --region ap-southeast-2 --repository-name "curtinmedicalcourseplanner/unitadmin" --query 'imageIds[*]' --output json )
-aws ecr batch-delete-image --region ap-southeast-2 --repository-name curtinmedicalcourseplanner/unitadmin --image-ids "$IMAGES_TO_DELETE" || true
-sam package --output-template-file packaged-template.yaml --image-repository 363837338544.dkr.ecr.ap-southeast-2.amazonaws.com/curtinmedicalcourseplanner/unitadmin --no-progressbar
-sam deploy --template ./packaged-template.yaml --stack-name UnitAdminAPIDev --region ap-southeast-2 --capabilities CAPABILITY_IAM --image-repository 363837338544.dkr.ecr.ap-southeast-2.amazonaws.com/curtinmedicalcourseplanner/unitadmin
-echo "Wiping ECR After Deploying..."
-aws ecr batch-delete-image --region ap-southeast-2 --repository-name curtinmedicalcourseplanner/unitadmin --image-ids "$IMAGES_TO_DELETE" || true # for some reason it doesnt need to exist, so we can save money by just removing it again
+if [ $? -eq 0 ]; then
+  echo "Beginning Deploy Stage"
+  echo "Wiping ECR Before Deploying..."
+  IMAGES_TO_DELETE=$( aws ecr list-images --region ap-southeast-2 --repository-name "curtinmedicalcourseplanner/unitadmin" --query 'imageIds[*]' --output json )
+  aws ecr batch-delete-image --region ap-southeast-2 --repository-name curtinmedicalcourseplanner/unitadmin --image-ids "$IMAGES_TO_DELETE" || true
+  sam package --output-template-file packaged-template.yaml --image-repository 363837338544.dkr.ecr.ap-southeast-2.amazonaws.com/curtinmedicalcourseplanner/unitadmin --no-progressbar
+  sam deploy --template ./packaged-template.yaml --stack-name UnitAdminAPIDev --region ap-southeast-2 --capabilities CAPABILITY_IAM --image-repository 363837338544.dkr.ecr.ap-southeast-2.amazonaws.com/curtinmedicalcourseplanner/unitadmin
+  echo "Wiping ECR After Deploying..."
+  aws ecr batch-delete-image --region ap-southeast-2 --repository-name curtinmedicalcourseplanner/unitadmin --image-ids "$IMAGES_TO_DELETE" || true # for some reason it doesnt need to exist, so we can save money by just removing it again
+else
+  echo "FAILED TESTS ENDING"
+fi
