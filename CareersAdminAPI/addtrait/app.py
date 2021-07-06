4a7e2b6c11a84b7c8f5f822e1b73a2447a6711c3
TorvaMessor159 <mattloe67@gmail.com>
2021-07-06 15:57:45 +0800

    Added check for auth and validation method started

--- CareersAdminAPI/addtrait/app.py
+++ CareersAdminAPI/addtrait/app.py
@@ -1,31 +1,51 @@
 import json
+import os
+import requests
+import jose
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    
+    bool valid
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    if (os.environ["DisableAuthentication"] == "true"):
+        print("Running with no authentication.")
+        valid = true
+    else:
+        token = event.get("Authorization")
+        valid = ValidateToken(token)
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    if (valid):
+        return {
+            "statusCode": 200,
+            
+            "body": json.dumps(
+            {
+                "message": "hello world",
+            }),
+        }
+    else:
+        return {
+            "statusCode": 401,
+            "headers": json.dumps(
+            {
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+                "Access-Control-Allow-Origin":  "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST"
+            }),
+            "body": json.dumps(
+            {
+                "message": "Failed authorization: " + err,
+            }),
+        }
+# 
+# Link: https://github.com/awslabs/aws-support-tools/blob/master/Cognito/decode-verify-jwt/decode-verify-jwt.py
+def ValidateToken(token):
+    keys_url = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_gn4KIEkx0/.well-known/jwks.json"
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    response = requests.get(keys_url)
+    keys = json.loads(response.decode('utf-8'))['keys']
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+    headers = jwt.get_unverified_headers(token)
+    kid = headers['kid']
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-        {
-            "message": "hello world",
-        }),
-    }
+    return valid, error
