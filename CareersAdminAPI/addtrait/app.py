09d5efb553ac55ee3ada8dd02a6f401ec88b4a61
Matthew Loe <mattloe67@gmail.com>
2021-07-12 14:29:58 +0800

    Add add to database code.

--- CareersAdminAPI/addtrait/app.py
+++ CareersAdminAPI/addtrait/app.py
@@ -1,20 +1,17 @@
 import json
 import os
-import requests
-import jose
+
+from .utils import Utils
 
 def lambda_handler(event, context):
+    utils = Utils()
     
-    bool valid
-
-    if (os.environ["DisableAuthentication"] == "true"):
-        print("Running with no authentication.")
-        valid = true
+    try:
+        utils.addToDatabase()
+    except Exception as err:
+        #Log error
+        pass
     else:
-        token = event.get("Authorization")
-        valid = ValidateToken(token)
-
-    if (valid):
         return {
             "statusCode": 200,
             
@@ -23,29 +20,3 @@ def lambda_handler(event, context):
                 "message": "hello world",
             }),
         }
-    else:
-        return {
-            "statusCode": 401,
-            "headers": json.dumps(
-            {
-                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-                "Access-Control-Allow-Origin":  "*",
-                "Access-Control-Allow-Methods": "OPTIONS,POST"
-            }),
-            "body": json.dumps(
-            {
-                "message": "Failed authorization: " + err,
-            }),
-        }
-# 
-# Link: https://github.com/awslabs/aws-support-tools/blob/master/Cognito/decode-verify-jwt/decode-verify-jwt.py
-def ValidateToken(token):
-    keys_url = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_gn4KIEkx0/.well-known/jwks.json"
-
-    response = requests.get(keys_url)
-    keys = json.loads(response.decode('utf-8'))['keys']
-
-    headers = jwt.get_unverified_headers(token)
-    kid = headers['kid']
-
-    return valid, error
--- CareersAdminAPI/addtrait/utils.py
+++ CareersAdminAPI/addtrait/utils.py
@@ -1,33 +1,55 @@
 import boto3
+import requests
+import json
+from jose import jwt
 from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
-client = boto3.client("dynamodb")
+class Utils:
+    def __init__(self):
+        self.db = boto3.resource('dynamodb', region_name="ap-southeast-2")
+        self.table = db.Table("Traits")
 
-#Http request data parsing
-def parseData(data):
-    print("Data parsing")
+    #Http request data parsing
+    def parseData(self, data):
+        print("Data parsing")
 
-#JWT token validation
-def validateJWTToken(token):
-    print("Token validation")
+    #JWT token validation
+    # Link: https://github.com/awslabs/aws-support-tools/blob/master/Cognito/decode-verify-jwt/decode-verify-jwt.py
+    def validateJWTToken(self, token):
+        keys_url = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_gn4KIEkx0/.well-known/jwks.json"
 
-#Database interactions
-def checkInDatabase(name):
-    
-    response = client.query(
-        TableName = '',
-        KeyConditionExpression = Key('name').eq(name)
-    )
+        response = requests.get(keys_url)
+        keys = json.loads(response.decode('utf-8'))['keys']
 
-def addToDatabase(name):
-    response = client.put_item(
-        TableName = '',
-        KeyConditionExpression = Key('name').eq(name)
-    )
+        headers = jwt.get_unverified_headers(token)
+        kid = headers['kid']
 
-#Http responses
-def badRequest():
-    print("badrequest")
+        return valid, error
 
-def okResponse():
-    print("ok")    
+    #Database interactions
+    def checkInDatabase(self, name):
+        try:
+            response = self.table.get_item(Key={'name': name})
+        except ClientError as err:
+            raise Exception("Failed to check for item in database.")
+        else:
+            if response:
+                return response['Item']
+            else:
+                return
+
+    def addToDatabase(self, name):
+        check = self.checkInDatabase(name)
+
+        if check:
+            raise Exception("Item already exists.")
+        else:
+            response = db.put_item(name) 
+
+    #Http responses
+    def badRequest(self):
+        print("badrequest")
+
+    def okResponse(self):
+        print("ok")    
