552c486c50aa03c85e6c579055cdc378c3f3b121
TorvaMessor159 <mattloe67@gmail.com>
2021-07-21 11:50:29 +0800

    Completed working addtrait function.

--- CareersAdminAPI/addtrait/app.py
+++ CareersAdminAPI/addtrait/app.py
@@ -1,8 +1,6 @@
 import json
-import os
 import boto3
 import requests
-from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
 
 #JWT token validation
@@ -11,57 +9,80 @@ def validateJWTToken(self, token):
     keys_url = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_gn4KIEkx0/.well-known/jwks.json"
 
     response = requests.get(keys_url)
-    keys = json.loads(response.decode('utf-8'))['keys']
+    keys = json.loads(response.decode("utf-8"))["keys"]
 
     #headers = jwt.get_unverified_headers(token)
-    #kid = headers['kid']
+    #kid = headers["kid"]
 
     #return valid, error
 
 def lambda_handler(event, context):
     #Setup link to database and table
-    db = boto3.resource('dynamodb', region_name="ap-southeast-2")
+    db = boto3.resource('dynamodb', region_name='ap-southeast-2')
     table = db.Table("DevTraits")
 
-    #Retrieve data
-    name = event['name']
-
-    #Check if item exists already else add
     try:
-        #Try get item from database
-        response = table.get_item(Key={'name': name})
-    except ClientError as err:
-        #Return error
-        return badRequest("Failed to check for item in database.")
-    else:
-        #Check for response
-        if response:
-            #Return successful request but response failure
-            return okResponse("Item already exists.")
-        else:
-            #Add item to database
-            response = db.put_item(name)
-            return okResponse("Trait successfully added to database.")
+        #Attempt to load table - checks if table exists
+        table.load()
+    except Exception:
+        #Create table
+        table = db.create_table(
+            TableName='DevTraits',
+            KeySchema=[
+                {
+                    'AttributeName': 'Name',
+                    'KeyType': 'HASH'
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'Name',
+                    'AttributeType': 'S'
+                }
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+        )
+
+        #Return bad request indicating table did not exist and is being created
+        return badRequest("Table did not exist. Table is now being created. Please try again later.")
+
+    #Retrieve data
+    body = json.loads(event["body"])   
+    name = body["name"]
+
+    #Add to table
+    #TODO - Add check for if it already exists
+    response = table.put_item(
+        Item={
+            "Name": name
+        }
+    )
+
+    #Return ok response
+    return okResponse("Trait added to database.")
 
 #Http responses
 #Badrequest response
-def badRequest( reason):
+def badRequest(reason):
     return {
         "statusCode" : 400,
         "body" : "Bad request: " + reason,
-        'headers': { 
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+        "headers": { 
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
             },
     }
 
 #Ok response
-def okResponse( reason):
+def okResponse(reason):
     return {
         "statusCode" : 200,
         "body" : "Success: " + reason,
-        'headers': { 
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+        "headers": { 
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
             },
     }    
