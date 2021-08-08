d4f590748165a9139310802b71c7716a6fbf0ff3
Reubs <reuben.whibley@student.curtin.edu.au>
2021-08-08 16:20:38 +0800

    Added docker configuration to all lambda functions, mark of finished productv1

---
+++ CareersApi/src/handlers/getcareer/GetCareer.js
@@ -0,0 +1,31 @@
+// Get the Careers DynamoDB table name from environment variables
+//import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+const tableName = process.env.DEV_CAREER_TABLE;
+
+const AWS = require('aws-sdk');
+AWS.config.update({
+    region: "ap-southeast-2"
+});
+
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.getCareer = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getCareer only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    // //TODO: Make this tolower when data has been changed to towerlower
+    let body = JSON.parse(event.body)
+    var params = {
+        TableName: tableName,
+        Key: {
+          'CareerId' : body['CareerId']
+        },
+    };
+    const item = await docClient.get(params).promise()
+
+    return {
+         statusCode: 200,
+         body: JSON.stringify(item)
+    }
+}
