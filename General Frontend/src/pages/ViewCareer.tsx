c6df2d2f7036c1e89cc7a68788919455711abee0
Andrew Li <andrewli-1920@hotmail.com>
2021-08-08 14:23:16 +0800

    more detailed and focused documentation

--- General Frontend/src/pages/ViewCareer.tsx
+++ General Frontend/src/pages/ViewCareer.tsx
@@ -2,6 +2,9 @@ import {Button, Typography} from "@material-ui/core";
 import Navbar from "../components/shared/Navbar";
 import { CareerProps } from "../types";
 
+// most likey used for top down, so user looks at careers, picks one (where though?) and a pre-planned plan is generated.
+// Pretty much just a white page with a title, paragraphs and a back button, could really be
+// with raw HTML tbh
 function ViewCareer(props: CareerProps) {
     return (
         <div>
