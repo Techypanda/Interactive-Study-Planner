a0784743eea55929f407daa573a976f7b187f5af
Andrew Li <andrewli-1920@hotmail.com>
2021-08-09 14:14:25 +0800

    Converted from css to scss

--- General Frontend/src/App.tsx
+++ General Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+@import './App.scss'; // this'll be deprecated by scss soon in favour of @use but that's not supported by CRA
 import Landing from './pages/Landing';
 
 // simply loads the landing page for now, could be used for other preliminary tasks later 
