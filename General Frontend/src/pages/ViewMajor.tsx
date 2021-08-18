ff23094002a4e7c0c97338c708a5d4723e154b42
TorvaMessor159 <mattloe67@gmail.com>
2021-08-18 22:17:24 +0800

    Added Major page and props, also added Id prop and career prop variable to Career page and text.

--- General Frontend/src/pages/ViewMajor.tsx
+++ General Frontend/src/pages/ViewMajor.tsx
@@ -1,42 +1,44 @@
 import {Button, Typography} from "@material-ui/core";
 import Navbar from "../components/shared/Navbar";
-import { CareerProps, DataId } from "../types";
+import { MajorProps, DataId } from "../types";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
-// most likey used for top down, so user looks at careers, picks one (where though?) and a pre-planned plan is generated.
+// most likey used for top down, so user looks at majors, picks one (where though?) and a pre-planned plan is generated.
 // Pretty much just a white page with a title, paragraphs and a back button, could really be
 // with raw HTML tbh
-function ViewCareer(props: DataId) {
-  //Get career information from table
-  const career : CareerProps = {
-    careerName : "Hello",
-    careerDescription : "ahfj",
-    careerIndustry : "ahgiod",
-    careerReqs : "dfiophjiah",
-    careerTraits : "aghoifh"
+function ViewMajor(props: DataId) {
+  //Get major information from table
+  const major : MajorProps = {
+    majorCode : "Hello",
+    majorName: "haoghoehg",
+    majorDescription : "ahfj",
+    majorUnits : "ahgiod",
+    majorPreReqs : "dfiophjiah",
+    majorCoReqs : "ajghoaeh",
+    majorAntiReqs : "aghoifh"
   };
 
   return (
       <div>
         <Navbar/>
         <Typography variant="h2">
-          {career.careerName}
+          {major.majorCode} - {major.majorName}
         </Typography>
         <Typography variant="h4">Description</Typography>
         <Typography variant="h6">
-          {career.careerDescription}
+          {major.majorDescription}
         </Typography>
         <Typography variant="h4">Industry</Typography>
         <Typography variant="h6">
-          {career.careerDescription}
+          {major.majorDescription}
         </Typography>
         <Typography variant="h4">Requirements</Typography>
         <Typography variant="h6">
-          {career.careerDescription}
+          {major.majorDescription}
         </Typography>
         <Typography variant="h4">Traits</Typography>
         <Typography variant="h6">
-          {career.careerDescription}
+          {major.majorDescription}
         </Typography>
         {/* why is material ui so inflexible and gross */}
         <Button variant="contained" style={{
@@ -48,4 +50,4 @@ function ViewCareer(props: DataId) {
   );
 }
 
-export default ViewCareer;
+export default ViewMajor;
--- General Frontend/src/types.d.ts
+++ General Frontend/src/types.d.ts
@@ -3,6 +3,10 @@ export interface DefaultProps {
   username?: string;
 }
 
+export interface DataId extends DefaultProps {
+  id: string;
+}
+
 export interface CareerEntryProps extends DefaultProps {
   careerTitle: string;
   careerContent: string;
@@ -14,6 +18,21 @@ export interface CareerProps extends DefaultProps {
   careerIndustry: string;
   careerReqs: string;
   careerTraits: string;
+}
+
+export interface MajorEntryProps extends DefaultProps {
+  majorTitle: string;
+  majorContent: string;
+}
+
+export interface MajorProps extends DefaultProps {
+  majorCode : string;
+  majorName : string;
+  majorDescription : string;
+  majorUnits : string;
+  majorPreReqs : string;
+  majorCoReqs : string;
+  majorAntiReqs : string;
 }
 
 export interface UnitProps extends DefaultProps {
