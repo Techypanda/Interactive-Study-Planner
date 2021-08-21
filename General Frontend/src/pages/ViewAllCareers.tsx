5130063ad6a4c682d2d8c5ef3d40374db782bc85
Andrew Li <andrewli-1920@hotmail.com>
2021-08-21 21:51:17 +0800

    Edits done to ViewAllCareers page, awaiting availability of careers api

--- General Frontend/src/pages/ViewAllCareers.tsx
+++ General Frontend/src/pages/ViewAllCareers.tsx
@@ -4,36 +4,43 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Box, Button, Container,Grid, Typography, Card, CardHeader, TextField} from "@material-ui/core";
 import Navbar from "../components/shared/Navbar";
 import styled from "styled-components";
+import {useHistory} from "react-router-dom";
 import { useQuery, useQueryClient } from "react-query";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { CareerProps, DefaultProps } from "../types";
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 // called on loading of this component
 // makes call to the careers API to get all careers so they can be rendered
+// props param is questionable atm but it's purpose is to take the form of the retrieved career
+// data which will be available to retreive once the CORS is figured out 
 function ViewAllCareers(props: DefaultProps) {
-    /* const { isLoading, isError, error, data } = useQuery('getcareers', async () => {
-       const data = await axios('${process.env.REACT_APP_CAREERS_API}/event-get-all-careers');
-       return data;
-     * })
-     * if (isLoading) {
-       return "Retrieving all careers";
-     * }
-     * if (isError) {
-       return error;
-     * } */
+    let history = useHistory();
+    /*
+    const { isLoading, isError, error, data } = useQuery('getcareers', async () => {
+	const data = await axios('${process.env.REACT_APP_CAREERS_API}/event-get-all-careers');
+	return data;
+    })
+    if (isLoading) {
+	return "Retrieving all careers";
+    }
+    if (isError) {
+	return error;
+    }*/
     return (
 	<>
 	    <Navbar />
-	    <Container id="listcareers" className={props.className}>
+	    <br/>
+	    <Container  >
 		<Box id="searchcontainer" display="flex">
-		    <TextField variant="outlined" placeholder="Search careers..." className="searchbar" />
-		    <Button color="primary" variant='contained' className="searchbtn">GO</Button>
+		    <TextField variant="outlined" id="standard-full-width" fullWidth  placeholder="Search careers..." className="searchbar" />
+		    <Button  variant='contained' className="searchbtn">Search</Button>
 		</Box>
 		<Box id="careers" marginTop={2}>
 		    
 		</Box>
 	    </Container>
-	    <Button>Back</Button>
+		<Button variant="contained" onClick={() => history.goBack()}>Back</Button>
 	</>
     );
 }
