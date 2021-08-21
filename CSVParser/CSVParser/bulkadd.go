fe88baa285ddabda2cb9eb54cbe41b1eb52a4016
Techypanda <Jonathan_wright@hotmail.com>
2021-08-21 13:28:41 +0800

    Unit & Majors processed in code

--- CSVParser/CSVParser/bulkadd.go
+++ CSVParser/CSVParser/bulkadd.go
@@ -13,6 +13,81 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 )
 
+func processMajor(scanner *bufio.Scanner, lineNumber *int) (Major, error) {
+	major := Major{}
+	*lineNumber += 1
+	read := scanner.Scan()
+	if !read {
+		return major, fmt.Errorf("expected majorcode on line: %d, recieved either error or eof", *lineNumber)
+	}
+	major.MajorCode = strings.ToUpper(scanner.Text())
+	*lineNumber += 1
+	read = scanner.Scan()
+	if !read {
+		return major, fmt.Errorf("expected majorname on line: %d, recieved either error or eof", *lineNumber)
+	}
+	major.Name = strings.ToUpper(scanner.Text())
+	*lineNumber += 1
+	read = scanner.Scan()
+	if !read {
+		return major, fmt.Errorf("Expected majorcredits on line: %d, recieved either error or eof", *lineNumber)
+	}
+	tempCredits, err := strconv.ParseFloat(scanner.Text(), 32)
+	if err != nil {
+		return major, fmt.Errorf("failed to parse credits on line: %d, error: %s", *lineNumber, err.Error())
+	}
+	major.Credits = float32(tempCredits)
+	*lineNumber += 1
+	read = scanner.Scan()
+	if !read {
+		return major, fmt.Errorf("Expected major units on line: %d, recieved either error or eof", *lineNumber)
+	}
+	temp := scanner.Text()
+	var majorUnits []string
+	for _, unit := range strings.Split(temp, ",") {
+		majorUnits = append(majorUnits, unit)
+	}
+	major.Units = majorUnits
+	*lineNumber += 1
+	read = scanner.Scan()
+	if !read {
+		return major, fmt.Errorf("Expected major unit antireqs on line: %d, recieved either error or eof", *lineNumber)
+	}
+	tempUnitAntiReqs := scanner.Text()
+	var unitAntiReqs [][]string
+	for _, unitAntiReqsGroup := range strings.Split(tempUnitAntiReqs, ";") {
+		var tempAntireqGroup []string
+		tempAntireqGroup = append(tempAntireqGroup, "")
+		for _, antiReq := range strings.Split(unitAntiReqsGroup, ",") {
+			tempAntireqGroup = append(tempAntireqGroup, antiReq)
+		}
+		unitAntiReqs = append(unitAntiReqs, tempAntireqGroup)
+	}
+	var tempAntireqGroup []string
+	tempAntireqGroup = append(tempAntireqGroup, "")
+	unitAntiReqs = append(unitAntiReqs, tempAntireqGroup)
+	major.UnitAntiReqs = unitAntiReqs
+	*lineNumber += 1
+	read = scanner.Scan()
+	if !read {
+		return major, fmt.Errorf("Expected major spec antireqs on line: %d, recieved either error or eof", *lineNumber)
+	}
+	tempSpecAntiReqs := scanner.Text()
+	var specAntiReqs [][]string
+	for _, specAntiReqsGroup := range strings.Split(tempSpecAntiReqs, ";") {
+		var tempAntireqGroup []string
+		tempAntireqGroup = append(tempAntireqGroup, "")
+		for _, antiReq := range strings.Split(specAntiReqsGroup, ",") {
+			tempAntireqGroup = append(tempAntireqGroup, antiReq)
+		}
+		specAntiReqs = append(specAntiReqs, tempAntireqGroup)
+	}
+	var tempSpecAntireqGroup []string
+	tempSpecAntireqGroup = append(tempSpecAntireqGroup, "")
+	specAntiReqs = append(specAntiReqs, tempSpecAntireqGroup)
+	major.SpecAntiReqs = specAntiReqs
+	return major, nil
+}
 func processUnit(scanner *bufio.Scanner, lineNumber *int) (Unit, error) {
 	unit := Unit{}
 	*lineNumber += 1
@@ -55,54 +130,57 @@ func processUnit(scanner *bufio.Scanner, lineNumber *int) (Unit, error) {
 		return unit, fmt.Errorf("expected prerequistes on line: %d, recieved either error or eof", *lineNumber)
 	}
 	tempPrereq := scanner.Text()
-	if tempPrereq == "" {
-		unit.Prerequistes = append(unit.Prerequistes, "")
-	} else {
-		for _, prereq := range strings.Split(tempPrereq, ",") {
-			for _, x := range unit.Prerequistes {
-				if strings.ToUpper(prereq) == x {
-					return unit, fmt.Errorf("duplicate prerequiste cannot exist, it occurs on line %d, value: %s", *lineNumber, prereq)
-				}
-			}
-			unit.Prerequistes = append(unit.Prerequistes, strings.ToUpper(prereq))
+	var preReqs [][]string
+	for _, preReqGroup := range strings.Split(tempPrereq, ";") {
+		var tempPrereqGroup []string
+		tempPrereqGroup = append(tempPrereqGroup, "")
+		for _, preReq := range strings.Split(preReqGroup, ",") {
+			tempPrereqGroup = append(tempPrereqGroup, preReq)
 		}
+		preReqs = append(preReqs, tempPrereqGroup)
 	}
+	var tempPrereqGroup []string
+	tempPrereqGroup = append(tempPrereqGroup, "")
+	preReqs = append(preReqs, tempPrereqGroup)
+	unit.Prerequistes = preReqs
 	*lineNumber += 1
 	read = scanner.Scan()
 	if !read {
 		return unit, fmt.Errorf("expected corequistes on line: %d, recieved either error or eof", *lineNumber)
 	}
 	tempCoreq := scanner.Text()
-	if tempCoreq == "" {
-		unit.Corequistes = append(unit.Corequistes, "")
-	} else {
-		for _, coreq := range strings.Split(tempCoreq, ",") {
-			for _, x := range unit.Corequistes {
-				if strings.ToUpper(coreq) == x {
-					return unit, fmt.Errorf("duplicate corequiste cannot exist, it occurs on line %d, value: %s", *lineNumber, coreq)
-				}
-			}
-			unit.Corequistes = append(unit.Corequistes, strings.ToUpper(coreq))
+	var coReqs [][]string
+	for _, coreqGroup := range strings.Split(tempCoreq, ";") {
+		var tempCoreqGroup []string
+		tempCoreqGroup = append(tempCoreqGroup, "")
+		for _, coReq := range strings.Split(coreqGroup, ",") {
+			tempCoreqGroup = append(tempCoreqGroup, coReq)
 		}
+		coReqs = append(coReqs, tempCoreqGroup)
 	}
+	var tempCoreqGroup []string
+	tempCoreqGroup = append(tempCoreqGroup, "")
+	coReqs = append(coReqs, tempCoreqGroup)
+	unit.Corequistes = coReqs
 	*lineNumber += 1
 	read = scanner.Scan()
 	if !read {
 		return unit, fmt.Errorf("expected antirequistes on line: %d, recieved either error or eof", *lineNumber)
 	}
-	tempAntiReq := scanner.Text()
-	if tempAntiReq == "" {
-		unit.Antirequistes = append(unit.Antirequistes, "")
-	} else {
-		for _, antireq := range strings.Split(tempAntiReq, ",") {
-			for _, x := range unit.Antirequistes {
-				if strings.ToUpper(antireq) == x {
-					return unit, fmt.Errorf("duplicate antirequiste cannot exist, it occurs on line %d, value: %s", *lineNumber, antireq)
-				}
-			}
-			unit.Antirequistes = append(unit.Antirequistes, strings.ToUpper(antireq))
+	tempAntireq := scanner.Text()
+	var antiReqs [][]string
+	for _, antiGroup := range strings.Split(tempAntireq, ";") {
+		var tempAntiGroup []string
+		tempAntiGroup = append(tempAntiGroup, "")
+		for _, antiReq := range strings.Split(antiGroup, ",") {
+			tempAntiGroup = append(tempAntiGroup, antiReq)
 		}
+		antiReqs = append(antiReqs, tempAntiGroup)
 	}
+	var tempAntiGroup []string
+	tempAntiGroup = append(tempAntiGroup, "")
+	antiReqs = append(antiReqs, tempAntiGroup)
+	unit.Antirequistes = antiReqs
 	return unit, nil
 }
 
@@ -117,9 +195,11 @@ func BulkAddEndpoint(body string) events.APIGatewayProxyResponse {
 	csvData, err := decodeBulkAdd(bulkReq.CSVData)
 	if err != nil {
 		log.Printf("Failed to decode base64 csv: %s", err.Error())
+		return BadRequest(fmt.Sprintf("Failed to decode base64 csv: %s", err.Error()))
 	}
 	log.Printf("Successfully processed data")
 	unitList := []Unit{}
+	majorList := []Major{}
 	scanner := bufio.NewScanner(strings.NewReader(csvData))
 	lineNumber := 0
 	for scanner.Scan() {
@@ -133,9 +213,24 @@ func BulkAddEndpoint(body string) events.APIGatewayProxyResponse {
 				return BadRequest(fmt.Sprintf("Failed to process a unit!: %s; On Line Number: %d", err.Error(), lineNumber))
 			}
 			unitList = append(unitList, unit)
+		case "MAJOR":
+			major, err := processMajor(scanner, &lineNumber)
+			if err != nil {
+				log.Printf("Failed to process a major!: %s; On Line Number: %d", err.Error(), lineNumber)
+				return BadRequest(fmt.Sprintf("Failed to process a unit!: %s; On Line Number: %d", err.Error(), lineNumber))
+			}
+			majorList = append(majorList, major)
+		case "":
+			continue
+		default:
+			log.Printf("Unknown data type: %s; On Line Number: %d", strings.ToUpper(dataType), lineNumber)
+			return BadRequest(fmt.Sprintf("Unknown data type: %s; On Line Number: %d", strings.ToUpper(dataType), lineNumber))
 		}
 	}
 	for _, i := range unitList {
+		fmt.Printf("%v", i)
+	}
+	for _, i := range majorList {
 		fmt.Printf("%v", i)
 	}
 	return OkResponse("Success - Bulk Addition")
--- CSVParser/CSVParser/structs.go
+++ CSVParser/CSVParser/structs.go
@@ -1,14 +1,34 @@
 package main
 
 type Unit struct {
-	UnitCode      string   `json:"unitCode"`
-	Name          string   `json:"unitName"`
-	Description   string   `json:"unitDescription"`
-	Credits       float32  `json:"unitCredits"` // could be a half unit :/
-	Delivery      string   `json:"delivery"`
-	Prerequistes  []string `json:"prerequistes"`
-	Corequistes   []string `json:"corequistes"`
-	Antirequistes []string `json:"antirequistes"`
+	UnitCode      string     `json:"unitCode"`
+	Name          string     `json:"unitName"`
+	Description   string     `json:"unitDescription"`
+	Credits       float32    `json:"unitCredits"` // could be a half unit :/
+	Delivery      string     `json:"delivery"`
+	Prerequistes  [][]string `json:"prerequistes"`
+	Corequistes   [][]string `json:"corequistes"`
+	Antirequistes [][]string `json:"antirequistes"`
+}
+
+type Major struct {
+	MajorCode    string     `json:"majorCode"`
+	Name         string     `json:"name"`
+	Credits      float32    `json:"credits"`
+	Units        []string   `json:"units"`
+	UnitAntiReqs [][]string `json:"unitAntiReqs"`
+	SpecAntiReqs [][]string `json:"specAntiReqs"`
+}
+
+type Specialization struct {
+	SpecCode       string     `json:"specCode"`
+	Name           string     `json:"name"`
+	Credits        float32    `json:"credits"`
+	CourseInternal bool       `json:"courseInternal"`
+	Units          []string   `json:"units"`
+	UnitAntiReqs   [][]string `json:"unitAntiReqs"`
+	SpecAntiReqs   [][]string `json:"specAntiReqs"`
+	MajorAntiReqs  [][]string `json:"majorAntiReqs"`
 }
 
 type BulkAddRequest struct {
