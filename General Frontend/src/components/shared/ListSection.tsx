5783f170c915caf5524cec73b3391c73f219f769
TorvaMessor159 <mattloe67@gmail.com>
2021-08-23 15:01:37 +0800

    Created list for displaying traits.

--- General Frontend/src/components/shared/ListSection.tsx
+++ General Frontend/src/components/shared/ListSection.tsx
@@ -1,16 +1,40 @@
-import { Box, Button, Paper, Typography } from "@material-ui/core";
-import ListDataProps from "../../types";
+import { List, ListItem, ListItemText, Typography } from "@material-ui/core";
+import { ListDataProps }  from "../../types";
 import styled from "styled-components";
 
 /*
  * Author: Matthew Loe
  * Student Id: 19452425
- * Date Last Modified: 22/08/2021
+ * Date Last Modified: 23/08/2021
  * Description: Component that returns a formated list section composed from list data.
  */
+
 function ListSection(props: ListDataProps)
 {
+    //Create list
+    const dataList = props.list.map(function(item)
+    {
+        return(
+            <ListItem>
+                <ListItemText primary={item} />
+            </ListItem>
+        );
+    });
 
+    return (
+        <div className={props.className}>
+            <Typography variant="h4" className="sectionHeading">
+                {props.sectionHeading}
+            </Typography>
+            <List>
+                {dataList}
+            </List>
+        </div>
+    );
 }
 
-export default ListSection;
+export default styled(ListSection)`
+.sectionHeading {
+    text-align: left;
+}
+`;
