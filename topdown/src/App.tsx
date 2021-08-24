128be9b1fb571a0dca33d433a5a376f164d53ec3
Max Woodcock <19153236@student.curtin.edu.au>
2021-08-24 15:43:25 +0800

    basic designs for now

--- topdown/src/App.tsx
+++ topdown/src/App.tsx
@@ -1,26 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import logo from './logo.svg';
+import  { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Box, Grid, Typography, Card, CardActions, CardActionArea, CardHeader, CardMedia, CardContent, Paper } from '@material-ui/core';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Navbar from './components/shared/Navbar'
+import CareersImage from '../src/static/career.jpg'
+import { DefaultProps } from "./types";
+//import { styled } from '@material-ui/styles';
+import styled from 'styled-components'
+import CareerUnitInfoCard from './components/shared/CareerUnitInfoCard';
+import Testing from './components/shared/Testing';
+import CurrentPlan from './components/shared/CurrentPlan';
+import AvailableCareersList from './components/shared/AvailableCareersList';
+import TopDownInitialMain from './components/shared/TopDownInitialMain';
+import TopDownFilledMain from './components/shared/TopDownFilledMain'
+const useStyles = makeStyles((theme) => ({ 
+  currentPlanList: { 
+    'z-index': 1
+  },
+  careersList: { 
+    'width': 300,
+  },
+  nonNavBar: {
+    'height': '90vh'
+  },
+  test: {
+    'margin-top': 50,
+  }
+}))
+function App(props: DefaultProps) { 
+  const classes = useStyles();
+  return ( 
+    <>
+      <Navbar />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Grid container justifyContent={'space-between'} alignItems={'stretch'} className={classes.nonNavBar}>
+        <Grid item xs={2}>
+          <CurrentPlan />
+        </Grid>
+        <Grid item xs={8} className={classes.test}>
+              <TopDownFilledMain />
+        </Grid>
+
+        <Grid item xs={2}>
+          <AvailableCareersList />
+        </Grid>
+      </Grid>
+    </>
+  
+  )
 }
-
-export default App;
+export default styled(App)`
+`;
