97ca4df31fd5d175bf5ca0796cf594618142d604
Max Woodcock <19153236@student.curtin.edu.au>
2021-08-24 16:55:36 +0800

    stashing :)) hehe

--- General Frontend/src/App.tsx
+++ General Frontend/src/App.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import './App.scss'; // this'll be deprecated by scss soon in favour of @use but that's not supported by CRA
 import Landing from './pages/Landing';
+//import TopdownFilled from '../pages/TopdownFilled';
 import TopdownFilled from './pages/TopdownFilled';
 
 // could just render the landing page from the index file but opportunities for other
