4f239644c38e0a8d74b6818aff19e8eea66ec1f4
Matthew Loe <mattloe67@gmail.com>
2021-08-25 15:37:46 +0800

    Updated name and finished routing.

--- General Frontend/src/components/shared/NavListSection.tsx
+++ General Frontend/src/components/shared/NavListSection.tsx
@@ -2,15 +2,17 @@ import { List, ListItem, ListItemIcon, ListItemText, Typography } from "@materia
 import { ListDataProps } from "../../types";
 import styled from "styled-components";
 import BulletPointIcon from "./BulletPointIcon";
+import { useHistory } from "react-router-dom";
 
 /*
  * Author: Matthew Loe
  * Student Id: 19452425
- * Date Last Modified: 24/08/2021
+ * Date Last Modified: 25/08/2021
  * Description: Component that returns a list section that allows each item to be clickable.
  */
 function InfoNav(props: String)
 {
+    const history = useHistory();
     const majorSpecHeads = ["MJRU", "SPUC"]
     //Retrieve first 4 characters of code
     const codeHead = props.substring(0,3);
@@ -18,23 +20,26 @@ function InfoNav(props: String)
     if (codeHead == majorSpecHeads[0])
     {
         //Nav to major info page
+        history.push('/ViewMajor')
         console.log("Nav to major");
     }
     else if (codeHead == majorSpecHeads[1])
     {
         //Nav to spec info page
+        history.push('/ViewSpecialisation')
         console.log("Nav to spec");
     }
     else
     {
         //Nav to unit info page
+        history.push('/ViewUnit')
         console.log("Nav to unit");
     }
     //END IF
 }
 
 //Returns list
-function ListSection(props: ListDataProps)
+function NavListSection(props: ListDataProps)
 {
     //Create list
     const dataList = props.list.map(function(item)
@@ -61,7 +66,7 @@ function ListSection(props: ListDataProps)
     );
 }
 
-export default styled(ListSection)`
+export default styled(NavListSection)`
 .sectionHeading {
     text-align: left;
     margin: 0px 0px 0px 14px;
