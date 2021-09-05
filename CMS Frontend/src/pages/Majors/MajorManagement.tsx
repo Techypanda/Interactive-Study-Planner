5787be6f1d4ea60a2f222b13bdf4fd9bd9d62e29
Techypanda <Jonathan_wright@hotmail.com>
2021-09-05 12:27:33 +0800

    Filtering Adding

--- CMS Frontend/src/pages/Majors/MajorManagement.tsx
+++ CMS Frontend/src/pages/Majors/MajorManagement.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography, Button, TextField, CircularProgress } from "@material-ui/core";
+import { Box, Container, Typography, Button, TextField, CircularProgress, Select, MenuItem } from "@material-ui/core";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
@@ -9,6 +9,19 @@ function MajorManagement(props: DefaultProps) {
   const history = useHistory();
   const [filtered, setFiltered] = useState<Array<Major>>()
   const majors = useMajors();
+  const [searchMode, setSearchMode] = useState<"Name" | "Description" | "MajorCode">("Name")
+  function filter(searchPhrase: string, field: "Name" | "Description" | "MajorCode" = "Name") {
+    const filteredSpecs: Array<Major> = []
+    const allLoadedUnits = majors.data?.data
+    if (allLoadedUnits) {
+      allLoadedUnits.forEach((major) => {
+        if (major[field].toUpperCase().includes(searchPhrase.toUpperCase())) {
+          filteredSpecs.push(major)
+        }
+      })
+    }
+    setFiltered(filteredSpecs)
+  }
   return (
     <Container id="majormanagement" className={props.className}>
       <Box id="titlebar" display="flex" justifyContent="center" alignItems="center" marginY={2}>
@@ -16,8 +29,20 @@ function MajorManagement(props: DefaultProps) {
         <Typography display="inline" variant="h4">{props.username} - Major Management</Typography>
       </Box>
       <Box id="searchcontainer" display="flex">
-        <TextField variant="outlined" placeholder="Search Units..." className="searchbar" />
-        <Button color="primary" variant='contained' className="searchbtn">GO</Button>
+        <TextField variant="outlined" placeholder="Search Units..." className="searchbar" onChange={(e) => filter(e.target.value, searchMode)} />
+        <Box marginLeft={2}>
+          <Select
+            id="select-searchmode"
+            value={searchMode}
+            variant="outlined"
+            className="gutHeight"
+            onChange={(e) => setSearchMode(e.target.value as "Name" | "Description" | "MajorCode")}
+          >
+            <MenuItem value={"Name"}>Name</MenuItem>
+            <MenuItem value={"Description"}>Description</MenuItem>
+            <MenuItem value={"MajorCode"}>Major Code</MenuItem>
+          </Select>
+        </Box>
       </Box>
       <Box id="units" marginTop={2}>
         {majors.isLoading
@@ -35,6 +60,9 @@ function MajorManagement(props: DefaultProps) {
 export default styled(MajorManagement)`
   #titlebar h4 {
     margin: auto;
+  }
+  #select-searchmode {
+    padding: 8.5px 34px 8.5px 24px !important;
   }
   #searchcontainer .searchbtn {
     margin-left: 15px;
--- CMS Frontend/src/pages/Specialization/SpecializationManagement.tsx
+++ CMS Frontend/src/pages/Specialization/SpecializationManagement.tsx
@@ -1,15 +1,27 @@
-import { Box, Container, Typography, Button, TextField, CircularProgress } from "@material-ui/core";
+import { Box, Container, Typography, Button, TextField, CircularProgress, Select, MenuItem } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import UnitEntry from "../../components/units/UnitEntry";
 import styled from "styled-components";
 import { useSpecializations } from "../../api/hooks";
 import PaginatedSpecializations from "../../components/specialization/PaginatedSpecializations";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function SpecializationManagement(props: DefaultProps) {
   const history = useHistory();
   const specs = useSpecializations()
   const [filtered, setFiltered] = useState<Array<Specialization>>()
+  const [searchMode, setSearchMode] = useState<"Name" | "Description" | "SpecializationCode">("Name")
+  function filter(searchPhrase: string, field: "Name" | "Description" | "SpecializationCode" = "Name") {
+    const filteredSpecs: Array<Specialization> = []
+    const allLoadedUnits = specs.data?.data
+    if (allLoadedUnits) {
+      allLoadedUnits.forEach((spec) => {
+        if (spec[field].toUpperCase().includes(searchPhrase.toUpperCase())) {
+          filteredSpecs.push(spec)
+        }
+      })
+    }
+    setFiltered(filteredSpecs)
+  }
   return (
     <Container id="specializationmanagement" className={props.className}>
       <Box id="titlebar" display="flex" justifyContent="center" alignItems="center" marginY={2}>
@@ -17,8 +29,20 @@ function SpecializationManagement(props: DefaultProps) {
         <Typography display="inline" variant="h4">{props.username} - Specialization Management</Typography>
       </Box>
       <Box id="searchcontainer" display="flex">
-        <TextField variant="outlined" placeholder="Search Units..." className="searchbar" />
-        <Button color="primary" variant='contained' className="searchbtn">GO</Button>
+        <TextField variant="outlined" placeholder="Search Units..." className="searchbar" onChange={(e) => filter(e.target.value, searchMode)} />
+        <Box marginLeft={2}>
+          <Select
+            id="select-searchmode"
+            value={searchMode}
+            variant="outlined"
+            className="gutHeight"
+            onChange={(e) => setSearchMode(e.target.value as "Name" | "Description" | "SpecializationCode")}
+          >
+            <MenuItem value={"Name"}>Name</MenuItem>
+            <MenuItem value={"Description"}>Description</MenuItem>
+            <MenuItem value={"SpecializationCode"}>Specialization Code</MenuItem>
+          </Select>
+        </Box>
       </Box>
       <Box id="units" marginTop={2}>
       {specs.isLoading
@@ -36,6 +60,9 @@ function SpecializationManagement(props: DefaultProps) {
 export default styled(SpecializationManagement)`
   #titlebar h4 {
     margin: auto;
+  }
+  #select-searchmode {
+    padding: 8.5px 34px 8.5px 24px !important;
   }
   #searchcontainer .searchbtn {
     margin-left: 15px;
