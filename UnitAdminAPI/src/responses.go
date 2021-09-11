67df4463d57e9f9b38f3a98709acd4d9fa93734d
Techypanda <Jonathan_wright@hotmail.com>
2021-09-11 10:52:06 +0800

    Fix errors + tests

--- UnitAdminAPI/src/responses.go
+++ UnitAdminAPI/src/responses.go
@@ -10,10 +10,11 @@ import (
 func BadRequest(reason string) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
 		Headers: map[string]string{
-			"Access-Control-Allow-Origin":  "*",
-			"Access-Control-Allow-Methods": "POST, OPTIONS",
-			"Access-Control-Allow-Headers": "true",
-			"Access-Control-Allow-Headers": "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Methods":     "POST, OPTIONS",
+			"Access-Control-Allow-Credentials": "true",
+			"Access-Control-Allow-Headers":     "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Content-Type":                     "application/json",
 		},
 		StatusCode: http.StatusBadRequest,
 		Body:       fmt.Sprintf("Bad Request: %s", reason),
@@ -23,10 +24,11 @@ func BadRequest(reason string) events.APIGatewayProxyResponse {
 func OkResponse(successText string) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
 		Headers: map[string]string{
-			"Access-Control-Allow-Origin":  "*",
-			"Access-Control-Allow-Methods": "POST, OPTIONS",
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Methods":     "POST, OPTIONS",
 			"Access-Control-Allow-Credentials": "true",
-			"Access-Control-Allow-Headers": "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Access-Control-Allow-Headers":     "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Content-Type":                     "application/json",
 		},
 		Body:       fmt.Sprintf("Success: %s", successText),
 		StatusCode: http.StatusOK,
@@ -36,10 +38,11 @@ func OkResponse(successText string) events.APIGatewayProxyResponse {
 func InvalidOperation(reason string) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
 		Headers: map[string]string{
-			"Access-Control-Allow-Origin":  "*",
-			"Access-Control-Allow-Methods": "POST, OPTIONS",
-			"Access-Control-Allow-Headers": "true",
-			"Access-Control-Allow-Headers": "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Methods":     "POST, OPTIONS",
+			"Access-Control-Allow-Credentials": "true",
+			"Access-Control-Allow-Headers":     "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Content-Type":                     "application/json",
 		},
 		Body:       fmt.Sprintf("We do not support: %s", reason),
 		StatusCode: http.StatusMethodNotAllowed,
@@ -49,10 +52,11 @@ func InvalidOperation(reason string) events.APIGatewayProxyResponse {
 func Unauthorized(reason string) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{
 		Headers: map[string]string{
-			"Access-Control-Allow-Origin":  "*",
-			"Access-Control-Allow-Methods": "POST, OPTIONS",
-			"Access-Control-Allow-Headers": "true",
-			"Access-Control-Allow-Headers": "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Access-Control-Allow-Origin":      "*",
+			"Access-Control-Allow-Methods":     "POST, OPTIONS",
+			"Access-Control-Allow-Credentials": "true",
+			"Access-Control-Allow-Headers":     "X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Requested-With,X-Auth-Token,Referer,User-Agent,Origin,Content-Type,Authorization,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
+			"Content-Type":                     "application/json",
 		},
 		Body:       fmt.Sprintf("You are not authorized: %s", reason),
 		StatusCode: http.StatusUnauthorized,
