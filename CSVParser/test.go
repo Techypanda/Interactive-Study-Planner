ac2661d10c5eeb13128f9ded32b68744ea449566
Techypanda <Jonathan_wright@hotmail.com>
2021-09-12 18:22:02 +0800

    Async sam tests

--- CSVParser/test.go
+++ CSVParser/test.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-	"bytes"
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 )
 
 func main() {
-	b, err := ioutil.ReadFile("BiomedicalUnits.csv")
+	b, err := ioutil.ReadFile("Test.csv")
 	if err != nil {
 		fmt.Print(err)
 	} else {
 		str := base64.StdEncoding.EncodeToString(b)
 		var jsonStr = []byte(fmt.Sprintf(`{ "csvData": "%s" }`, str))
-		req, err := http.NewRequest("POST", "http://localhost:3000/add", bytes.NewBuffer(jsonStr))
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println("repsonse body:", string(body))
+		fmt.Println(string(jsonStr))
+		// req, err := http.NewRequest("POST", "http://localhost:3000/add", bytes.NewBuffer(jsonStr))
+		// client := &http.Client{}
+		// resp, err := client.Do(req)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// defer resp.Body.Close()
+		// body, _ := ioutil.ReadAll(resp.Body)
+		// fmt.Println("repsonse body:", string(body))
 	}
 }
