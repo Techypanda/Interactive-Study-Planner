71b22d186ad268d91c37e11f64295b426754d51f
Andrew Li <andrewli-1920@hotmail.com>
2021-09-14 22:53:41 +0800

    Rudimentary components of bottom up done, TODO: prettify it, speed up rendering of the units list because 100 or so list components should be instant, convert to draggable types and test _extensively_

--- General Frontend/src/pages/CoursePlanner.tsx
+++ General Frontend/src/pages/CoursePlanner.tsx
@@ -1,9 +1,12 @@
 import EmptyCurrentPlan from "../components/shared/EmptyCurrentPlan";
 import LoadingScreen from "../components/shared/Loading";
 import Navbar from "../components/shared/Navbar";
+import PlansAvailable from "../components/shared/PlansAvailable";
 import { DefaultProps } from "../types";
 import Error from "../components/shared/Error";
-import { Typography, List, ListItem, ListItemText, Grid } from "@material-ui/core";
+import { Typography, List, ListItem, ListItemText, ListItemSecondaryAction, Grid } from "@material-ui/core";
+import BookOutlined from '@material-ui/icons/BookOutlined';
+import IconButton from '@material-ui/core/IconButton';
 import { useQuery, useMutation } from "react-query";
 import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 import { UnitProps } from '../types';
@@ -38,21 +41,22 @@ function CoursePlanner() {
     const classes = useStyles();
     const history = useHistory();
 
-    const [units_list, set_units_list] = useState(null);
+    const base = [{Credits: "", Antirequisites: "", Prerequisites: "", Delivery: "", UnitCode: "", Description: "", Corequisites: "", Name: ""}];
+    const [units_list, set_units_list] = useState(base);
 
     const [isError, setError ] = useState(false);
     const [error, setErrorContent] = useState<string>();
 
     
     // updated as units are selected so that available units can be filtered out
-    const [prerequisites_list, set_prerequisites_list] = useState(" ");
+    const [prereqs_list, set_prereqs_list] = useState();
 
     const GetAllUnits = () => {
 	try {
 	    axios.get("https://ilur318q9c.execute-api.ap-southeast-2.amazonaws.com/Prod/getallunits")
-		.then((response) => {
-		    console.log(response.data);
-		    set_units_list(response.data);
+		 .then((response) => {
+		     console.log(response.data);
+		     set_units_list(response.data);
 		 });
 	} catch (err) {
 	    if (err && err.response && axios.isAxiosError(err)) {
@@ -73,25 +77,20 @@ function CoursePlanner() {
     if (isError) {
 	return <Error promptTitle="Request Error" promptContent={error as string} showPrompt={true} onAccept={() => history.goBack()} />
     }
-    
+
     return (
 	<>
 	    <Navbar/>
-	    <Typography variant="h4" align="left">
-		{prerequisites_list}
-	    </Typography>
-	    
 	    <DragDropContext
 		onDragUpdate={OnDragUpdate}
 		onDragEnd={OnDragEnd}>
 		<Grid container justifyContent={'space-between'} alignItems={'stretch'} className={classes.nonNavBar}>
 		    <Grid item xs={2}>
-			<EmptyCurrentPlan/> {/* placeholder */}
+			<EmptyCurrentPlan/> 
 		    </Grid>
 		    <Grid item xs={8} className={classes.test}>
 			<div>
-
-
+			    <PlansAvailable/>
 			</div>
 		    </Grid>
 		    <Grid item xs={2}>
@@ -112,23 +111,38 @@ function UnitDraggableItem() {
 
 // scrollable list to the right of the screen, dynamically shows available courses
 // as pre-reqs and anti-reqs are made, rendering and unrendering as necessary.
-function AvailableUnitsDisplay({units_list} : {units_list: any}) {
+function AvailableUnitsDisplay({units_list} : {units_list : any}) {
     const classes = useStyles();
     return (
-	<Typography>hey</Typography>
-    );
+	/* will be made into a droppable area
+	   TODO: unit items to become UnitDraggableItem types rather than simple list items */
+	<List className={classes.root}>
+	    {units_list.map((value: any) => {
+		return (
+		    <ListItem role={undefined}>
+			<ListItemText primary={value.Name} />
+			<ListItemSecondaryAction>
+                        <IconButton edge='end' aria-label='comments'>
+			        <BookOutlined />
+                        </IconButton>
+			</ListItemSecondaryAction>
+	            </ListItem>
+		)
+	    })}
+	</List>
+    )
 }
 
 // displays selected courses drag and dropped
 // divided into semesters
-function SelectionArea() {
+function SelectedUnitsDisplay() {
     return (
 	<Droppable droppableId="droppable">
 	    {(provided, snapshot) =>(
 		<div
 		    {...provided.droppableProps}
 		    ref={provided.innerRef}>
-
+		    
 
 		    {provided.placeholder}
 		    
@@ -171,16 +185,25 @@ function OnDragEnd(result: DropResult) {
     return;
 }
 
-function CheckAntiRequisites(antirequisites: string[]) {
-    // if any of the param's elements are present in the current prerequisite pool or selection pool, return false
-}
+// following is likely not going to be used
+// filtering is done on change detected in the selected area to re-render available units section, compatibility is NOT
+// checked upon drop attempt
 
-function CheckPrerequisites(prerequisites: string[]) {
-    // entire prereq array must be present within prerequisite state hook storage
-}
-
-function CheckCorequisites(corequisites: string[]) {
-    
-}
+/* temporary, will replace with more conventional string array sharing likely with useContext() or global state or useQuery() */
+/* function CompatibleAntiRequisites(curr_prereqs: string[], antireqs: string[]) {
+ *     // if any of the param's elements are present in the current prerequisite pool or selection pool, return false
+ *     var shared_reqs = curr_prereqs.filter(element => antireqs.includes(element));
+ *     return shared_reqs.length === 0 ? true : false;
+ * }
+ * 
+ * function CompatiblePrerequisites(curr_prereqs: string[], selected_unit_prereqs: string[]) {
+ *     // entire prereq array must be present within prerequisite state hook 
+ *     var shared_reqs = curr_prereqs.filter(element => selected_unit_prereqs.includes(element));
+ *     return shared_reqs.length === selected_unit_anitreqs ? true : false;
+ * }
+ * 
+ * function CompatibleCorequisites(curr_prereqs: string[], coreqs: string[]) {
+ *     // For the coreqs to be compatible, the units in the coreqs array must be composed of classes already in that semester
+ * } */
 
 export default CoursePlanner;
