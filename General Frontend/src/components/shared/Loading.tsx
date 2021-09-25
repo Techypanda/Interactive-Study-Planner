73da6627a9154eb7a879283a92f57c19ebe0e1ac
Techypanda <Jonathan_wright@hotmail.com>
2021-09-25 15:13:26 +0800

    navbar removal

--- General Frontend/src/components/shared/Loading.tsx
+++ General Frontend/src/components/shared/Loading.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Box, Typography } from "@material-ui/core";
-import Navbar from "./Navbar";
 
 // plays animation and text for when the data is being retrieved should it be that slow
 function LoadingScreen() {
     return (
         <>
-          <Navbar/>
           <Box>
             <Typography variant="h2"> Retrieving data </Typography>
           </Box>
