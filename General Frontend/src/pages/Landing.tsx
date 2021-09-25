f596961a84e4e0b7f24a29574fb1e9825971ef03
Techypanda <Jonathan_wright@hotmail.com>
2021-09-25 14:51:38 +0800

    Landing now follows material ui and is mobile responsive

--- General Frontend/src/pages/Landing.tsx
+++ General Frontend/src/pages/Landing.tsx
@@ -1,39 +1,55 @@
-import { Card, CardHeader, CardContent, Grid, Typography, Container } from "@material-ui/core";
+import { Card, CardHeader, CardContent, Grid, Typography, Container, Box, Paper, useMediaQuery } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 
 // stock images
 import MedicalImage from "../static/doctor_teacher.jpg";
 import CareersImage from "../static/career.jpg";
+import CardPrompt from "../components/Landing/CardPrompt";
 
 
 
 function Landing() {
 	const history = useHistory();
+	const isTablet = useMediaQuery('(max-width: 800px)');
 	return (
 		<>
 			<Container>
-				<Grid container spacing={8} justify="center">
-					<Grid item xs={12} sm={6}>
-						<Typography variant="h5">
-							Hi, welcome to Curtin University's Medical Course Planner!
-						</Typography>
+				<Box mt={4.5} mb={2}>
+					<Typography variant="h5" component="h2">
+						Hi, Welcome to Curtin University's Medical Course Planner
+					</Typography>
+				</Box>
+				<Box mb={isTablet ? 3 : 10}>
+					<Typography variant="subtitle1" component="h3" className="classes.prompt">
+						What Would You Like To Do?
+					</Typography>
+				</Box>
+				<Grid container>
+					<Grid item md={6} xs={12}>
+						<Box mb={isTablet ? 2 : 0}>
+							<CardPrompt
+								onClick={() => history.push('./PlannerInitialPage')}
+								src={MedicalImage}
+								alt="A Doctor Pointing At A Whiteboard"
+								title={"Plan My Medical Course"}
+								description={"See how Curtin's flexible course structure can be shaped to you advantage."}
+							/>
+						</Box>
+					</Grid>
+					<Grid item md={6} xs={12}>
+						<Box mb={isTablet ? 2 : 0}>
+							<CardPrompt
+								onClick={() => history.push('/ViewAllCareers')}
+								src={CareersImage}
+								alt="A Stock Image Declaring Keywords Around Dream Jobs"
+								title={"See Available Careers"}
+								description={"See all the possible careers Curtin can lead to and their requirements."}
+							/>
+						</Box>
 					</Grid>
 				</Grid>
-				<br />
-				<br />
-				<br />
-				<Typography variant="h5" className="classes.prompt">
-					What would you like to do?
-				</Typography>
-				<br>
-				</br>
-				<br>
-				</br>
-				<br>
-				</br>
 
-				{/* routed cards */}
-				<Grid container spacing={4} justify="center">
+				{/* <Grid container spacing={4} justify="center">
 					<Grid item xs={12} sm={6}>
 						<div className="card-body">
 							<Card variant="outlined" onClick={() => history.push('./PlannerInitialPage')}>
@@ -65,8 +81,7 @@ function Landing() {
 							</Card>
 						</div>
 					</Grid>
-
-				</Grid>
+				</Grid> */}
 			</Container>
 		</>
 	);
