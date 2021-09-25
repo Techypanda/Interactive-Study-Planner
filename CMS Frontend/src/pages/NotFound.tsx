d11c684fb569bfc5fdcf1fa0214b1d70d1c6ed87
Techypanda <Jonathan_wright@hotmail.com>
2021-09-25 14:00:48 +0800

    New not found page

--- CMS Frontend/src/pages/NotFound.tsx
+++ CMS Frontend/src/pages/NotFound.tsx
@@ -1,12 +1,28 @@
-import { Box, Button, Typography } from "@material-ui/core";
+import { Box, Button, Container, Typography } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-
-export default function NotFound(props: DefaultProps) {
+import styled from "styled-components";
+function NotFound(props: DefaultProps) {
   const history = useHistory();
   return (
-    <Box>
-      <Typography variant="h2">Sorry, that page wasn't found</Typography>
-      <Button onClick={() => history.push('/')}>Return To Landing</Button>
-    </Box>    
+    <Box className={props.className} display="flex" justifyContent="center" top={0} left={0} position="absolute" height="100vh" width="100vw" alignItems="center">
+      <Container>
+        <Typography variant="h1" align="center">404</Typography>
+        <Typography variant="h4" className="subtext" align="center">The page you are looking for does not exist.</Typography>
+        <Typography variant="subtitle1" className="subtext" align="center">(Unless you were looking for the 404 page, then it does exist)</Typography>
+        <Box display="flex" justifyContent="center" mt={2}>
+          <Button variant="contained" color="secondary" onClick={() => history.push('/')}>Go Home</Button>
+        </Box>
+      </Container>
+    </Box>
   )
 }
+export default styled(NotFound)`
+.logo {
+  object-fit: contain;
+  max-width: 450px !important;
+  width: 100%;
+}
+.subtext {
+  font-weight: 300;
+}
+`;
