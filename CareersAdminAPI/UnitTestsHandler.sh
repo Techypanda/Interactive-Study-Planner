dc9ab2b0ee1c0cfe1bd3ea10684d953f445aed13
Matthew Loe <mattloe67@gmail.com>
2021-10-04 20:48:35 +0800

    Commented out trait functions and tests.

--- CareersAdminAPI/UnitTestsHandler.sh
+++ CareersAdminAPI/UnitTestsHandler.sh
@@ -13,37 +13,37 @@ rm -f -- UnitTestOutput.txt
 echo "Running Build" >> UnitTestOutput.txt
 sam build --parallel >> UnitTestOutput.txt
 
-#Testing trait api and functions
-echo "Running Trait Api Events" >> UnitTestOutput.txt
-echo "Add Trait Api" >> UnitTestOutput.txt
-#Executing Add Trait Async
-sam local invoke --env-vars unittests/debug.json AddTraitFunction -e unittests/payloads/trait/AddTraitEvent.json > unittests/responses/trait/AddTraitResponse.json &
-addtrait=$!
+# #Testing trait api and functions
+# echo "Running Trait Api Events" >> UnitTestOutput.txt
+# echo "Add Trait Api" >> UnitTestOutput.txt
+# #Executing Add Trait Async
+# sam local invoke --env-vars unittests/debug.json AddTraitFunction -e unittests/payloads/trait/AddTraitEvent.json > unittests/responses/trait/AddTraitResponse.json &
+# addtrait=$!
 
-echo "Update Trait Non-Existing Item" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json UpdateTraitFunction -e unittests/payloads/trait/UpdateNonExistTraitEvent.json > unittests/responses/trait/UpdateNonExistResponse.json &
-updatenoexisttrait=$!
+# echo "Update Trait Non-Existing Item" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json UpdateTraitFunction -e unittests/payloads/trait/UpdateNonExistTraitEvent.json > unittests/responses/trait/UpdateNonExistResponse.json &
+# updatenoexisttrait=$!
 
-echo "Delete Trait Non-Existing Item" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json DeleteTraitFunction -e unittests/payloads/trait/DeleteTraitEventNoExist.json > unittests/responses/trait/DeleteNonExistResponse.json & # uses separate json now to avoid race condition
-deletenoexist=$! 
+# echo "Delete Trait Non-Existing Item" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json DeleteTraitFunction -e unittests/payloads/trait/DeleteTraitEventNoExist.json > unittests/responses/trait/DeleteNonExistResponse.json & # uses separate json now to avoid race condition
+# deletenoexist=$! 
 
-echo "Attempt Add Trait with Bad Input" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json AddTraitFunction -e unittests/payloads/trait/BadInputEvent.json > unittests/responses/trait/AddBadInputResponse.json &
-badinputtrait=$!
+# echo "Attempt Add Trait with Bad Input" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json AddTraitFunction -e unittests/payloads/trait/BadInputEvent.json > unittests/responses/trait/AddBadInputResponse.json &
+# badinputtrait=$!
 
-echo "Attempt Update Trait with Bad Input" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json UpdateTraitFunction -e unittests/payloads/trait/BadInputEvent.json > unittests/responses/trait/UpdateBadInputResponse.json &
-badinputupdate=$!
+# echo "Attempt Update Trait with Bad Input" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json UpdateTraitFunction -e unittests/payloads/trait/BadInputEvent.json > unittests/responses/trait/UpdateBadInputResponse.json &
+# badinputupdate=$!
 
-echo "Attempt Delete Trait with Bad Input" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json DeleteTraitFunction -e unittests/payloads/trait/BadInputEvent.json > unittests/responses/trait/DeleteBadInputResponse.json &
-badinputdeletetrait=$!
+# echo "Attempt Delete Trait with Bad Input" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json DeleteTraitFunction -e unittests/payloads/trait/BadInputEvent.json > unittests/responses/trait/DeleteBadInputResponse.json &
+# badinputdeletetrait=$!
 
 #Testing Career api and functions
 echo "Running Career Events"
@@ -80,23 +80,23 @@ deletecareerbad=$!
 # WAITS
 echo "Waiting all all seeding data"
 wait
-#Comparing expected response to actual response
-diff unittests/responses/trait/AddTraitResponse.json unittests/expectedResponses/trait/AddTraitExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/AddTraitResponse.json unittests/expectedResponses/trait/AddTraitExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/UpdateNonExistResponse.json unittests/expectedResponses/trait/NonExistExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/UpdateNonExistResponse.json unittests/expectedResponses/trait/NonExistExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/DeleteTraitResponse.json unittests/expectedResponses/trait/DeleteTraitExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/DeleteTraitResponse.json unittests/expectedResponses/trait/DeleteTraitExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/AddBadInputResponse.json unittests/expectedResponses/trait/BadInputExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/AddBadInputResponse.json unittests/expectedResponses/trait/BadInputExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/UpdateBadInputResponse.json unittests/expectedResponses/trait/BadInputExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/UpdateBadInputResponse.json unittests/expectedResponses/trait/BadInputExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/DeleteBadInputResponse.json unittests/expectedResponses/trait/BadInputExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/DeleteBadInputResponse.json unittests/expectedResponses/trait/BadInputExpectedResponse.json >> UnitTestOutput.txt
 
 #Comparing expected response to actual response
 diff unittests/responses/career/AddCareerResponse.json unittests/expectedResponses/career/AddCareerExpectedResponse.json >> UnitTestOutput.txt
@@ -117,19 +117,19 @@ diff unittests/responses/career/UpdateBadInputResponse.json unittests/expectedRe
 diff unittests/responses/career/DeleteBadInputResponse.json unittests/expectedResponses/career/BadInputExpectedResponse.json >> UnitTestOutput.txt
 
 echo "Running Add Event That Relied On Data since adds are done"
-echo "Add Trait Already Existing Item" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json AddTraitFunction -e unittests/payloads/trait/AddTraitEvent.json > unittests/responses/trait/AlreadyExistsResponse.json &
-addtraitexists=$!
+# echo "Add Trait Already Existing Item" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json AddTraitFunction -e unittests/payloads/trait/AddTraitEvent.json > unittests/responses/trait/AlreadyExistsResponse.json &
+# addtraitexists=$!
 
 echo "Add Career Already Existing Item" >> UnitTestOutput.txt
 #Executing
 sam local invoke --env-vars unittests/debug.json AddCareerFunction -e unittests/payloads/career/AddCareerEvent.json > unittests/responses/career/AlreadyExistsResponse.json &
 addcareerexists=$!
 
-wait
-#Comparing expected response to actual response
-diff unittests/responses/trait/AlreadyExistsResponse.json unittests/expectedResponses/trait/AlreadyExistsExpectedResponse.json >> UnitTestOutput.txt
+# wait
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/AlreadyExistsResponse.json unittests/expectedResponses/trait/AlreadyExistsExpectedResponse.json >> UnitTestOutput.txt
 
 #Comparing expected response to actual response
 diff unittests/responses/career/AlreadyExistsResponse.json unittests/expectedResponses/career/AlreadyExistsExpectedResponse.json >> UnitTestOutput.txt
@@ -140,23 +140,23 @@ echo "Update Career Api" >> UnitTestOutput.txt
 sam local invoke --env-vars unittests/debug.json UpdateCareerFunction -e unittests/payloads/career/UpdateCareerEvent.json > unittests/responses/career/UpdateCareerResponse.json &
 updatecareer=$!
 
-echo "Update Trait Api" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json UpdateTraitFunction -e unittests/payloads/trait/UpdateTraitEvent.json > unittests/responses/trait/UpdateTraitResponse.json &
-updatetrait=$!
+# echo "Update Trait Api" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json UpdateTraitFunction -e unittests/payloads/trait/UpdateTraitEvent.json > unittests/responses/trait/UpdateTraitResponse.json &
+# updatetrait=$!
 
 wait
 #Comparing expected response to actual response
 diff unittests/responses/career/UpdateCareerResponse.json unittests/expectedResponses/career/UpdateCareerExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/UpdateTraitResponse.json unittests/expectedResponses/trait/UpdateTraitExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/UpdateTraitResponse.json unittests/expectedResponses/trait/UpdateTraitExpectedResponse.json >> UnitTestOutput.txt
 
 echo "Running Deletes now that all of the adds are done"
-echo "Delete Trait Api" >> UnitTestOutput.txt
-#Executing
-sam local invoke --env-vars unittests/debug.json DeleteTraitFunction -e unittests/payloads/trait/DeleteTraitEvent.json > unittests/responses/trait/DeleteTraitResponse.json &
-deletetrait=$!
+# echo "Delete Trait Api" >> UnitTestOutput.txt
+# #Executing
+# sam local invoke --env-vars unittests/debug.json DeleteTraitFunction -e unittests/payloads/trait/DeleteTraitEvent.json > unittests/responses/trait/DeleteTraitResponse.json &
+# deletetrait=$!
 
 echo "Delete Career Api" >> UnitTestOutput.txt
 #Executing
@@ -167,5 +167,5 @@ wait
 #Comparing expected response to actual response
 diff unittests/responses/career/DeleteCareerResponse.json unittests/expectedResponses/career/DeleteCareerExpectedResponse.json >> UnitTestOutput.txt
 
-#Comparing expected response to actual response
-diff unittests/responses/trait/DeleteNonExistResponse.json unittests/expectedResponses/trait/NonExistExpectedResponse.json >> UnitTestOutput.txt
+# #Comparing expected response to actual response
+# diff unittests/responses/trait/DeleteNonExistResponse.json unittests/expectedResponses/trait/NonExistExpectedResponse.json >> UnitTestOutput.txt
