8dbb96ae5cfa82d523ae3e3eab7be2e6a472676a
Techypanda <Jonathan_wright@hotmail.com>
2021-10-07 11:04:58 +0800

    fix act

--- General Frontend/src/pages/ViewAllUnits.tsx
+++ General Frontend/src/pages/ViewAllUnits.tsx
@@ -1,67 +1,68 @@
 import { useEffect, useState } from 'react';
-import { Container} from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { DefaultProps } from "../types";
-import { createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import UnitsDataTable from "../components/shared/UnitsDatatable"
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-	root: {
-	    display: 'flex',
-	},
-	details: {
-	    display: 'flex',
-	    flexDirection: 'column',
-	},
-	content: {
-	    flex: '1 0 auto',
-	},
-	cover: {
-	    width: 151,
-	},
-	controls: {
-	    display: 'flex',
-	    alignItems: 'right',
-	    paddingRight: theme.spacing(1),
-	    paddingBottom: theme.spacing(1),
-	},
-	playIcon: {
-	    height: 38,
-	    width: 38,
-	},
-	
-    }),
+	createStyles({
+		root: {
+			display: 'flex',
+		},
+		details: {
+			display: 'flex',
+			flexDirection: 'column',
+		},
+		content: {
+			flex: '1 0 auto',
+		},
+		cover: {
+			width: 151,
+		},
+		controls: {
+			display: 'flex',
+			alignItems: 'right',
+			paddingRight: theme.spacing(1),
+			paddingBottom: theme.spacing(1),
+		},
+		playIcon: {
+			height: 38,
+			width: 38,
+		},
+
+	}),
 );
 
+
 function ViewAllUnits(props: DefaultProps) {
-    const history = useHistory();
-    const classes = useStyles();
+	const history = useHistory();
+	const classes = useStyles();
 
-    const base = [{Credits: "", Antirequisites: "", Prerequisites: "", Delivery: "", UnitCode: "", Description: "", Corequisites: "", Name: ""}];
-    const [units_list, set_units_list] = useState(base);
+	const base = [{ Credits: "", Antirequisites: "", Prerequisites: "", Delivery: "", UnitCode: "", Description: "", Corequisites: "", Name: "" }];
+	const [units_list, set_units_list] = useState(base);
 
-    const GetAllUnits = () => {
-	axios.get("https://ilur318q9c.execute-api.ap-southeast-2.amazonaws.com/Prod/getallunits/")
-	.then((response) => {
-	    act(set_units_list(response.data));
-	});
-    }
-    useEffect(() => {
-	GetAllUnits();
-    }, []);
+	const GetAllUnits = () => {
+		axios.get("https://ilur318q9c.execute-api.ap-southeast-2.amazonaws.com/Prod/getallunits/")
+			.then((response) => {
+				set_units_list(response.data);
+			});
+	}
+	useEffect(() => {
+		GetAllUnits();
+	}, []);
 
-    return (
-	<>
-		<br/>
-	    <Container  >
-			<UnitsDataTable items={units_list}/>
-		</Container>
-	    <br/>
+	return (
+		<>
+			<br />
+			<Container  >
+				<UnitsDataTable items={units_list} />
+			</Container>
+			<br />
 
-	</>
-    );
+		</>
+	);
 }
 
 export default ViewAllUnits;
