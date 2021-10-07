05fc53b1ad55a399e1af49beee75909ba3bb0716
Techypanda <Jonathan_wright@hotmail.com>
2021-10-07 12:26:13 +0800

    Introduce caching + nicer code for view all careers

--- General Frontend/src/pages/ViewAllCareers.tsx
+++ General Frontend/src/pages/ViewAllCareers.tsx
@@ -1,61 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Container } from "@material-ui/core";
-import axios from "axios";
-
 import { DefaultProps } from "../types";
 import CareersDatatable from "../components/shared/CareersDatatable"
 import { BounceLoader } from "react-spinners";
-/* const useStyles = makeStyles((theme: Theme) =>
-		createStyles({
-	root: {
-			display: 'flex',
-	},
-	details: {
-			display: 'flex',
-			flexDirection: 'column',
-	},
-	content: {
-			flex: '1 0 auto',
-	},
-	cover: {
-			width: 151,
-	},
-	controls: {
-			display: 'flex',
-			alignItems: 'right',
-			paddingRight: theme.spacing(1),
-			paddingBottom: theme.spacing(1),
-	},
-	playIcon: {
-			height: 38,
-			width: 38,
-	},
-	
-		}),
-); */
+import { useCareers } from '../api/hooks';
 
 function ViewAllCareers(props: DefaultProps) {
-
-	const base = [{ Description: "", Industry: "", CareerId: "" }]
-	const [careersList, setCareersData] = useState(base);
-	const [loading, setLoading] = useState(true);
-
-	const getCareersData = () => {
-		axios.get("https://q02l9qoni6.execute-api.ap-southeast-2.amazonaws.com/Prod/events/event-get-all-careers")
-			.then((response) => {
-				setCareersData(response.data);
-				setLoading(false)
-			})
-	}
-	useEffect(() => {
-		getCareersData();
-	}, []);
-
+	const careers = useCareers();
 	return (
 		<>
 			<br />
 			<Container  >
-				{!loading ? <CareersDatatable items={careersList} /> : <BounceLoader color="#1473AB" loading={true} size={150} />}
+				{!careers.isLoading ? <CareersDatatable items={careers.data?.data} /> : <BounceLoader color="#1473AB" loading={true} size={150} />}
 			</Container>
 			<br />
 		</>
